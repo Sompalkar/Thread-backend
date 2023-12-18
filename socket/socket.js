7fc70474fc8e800eb32efc9c89ed77fc2a818252
Som Palkar <132274403+Sompalkar@users.noreply.github.com>
2023-12-18 11:03:53 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: "https://thread-chat-com.vercel.app",
 		methods: ["GET", "POST"],
 	},
 });
